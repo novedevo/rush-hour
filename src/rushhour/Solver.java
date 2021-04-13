4b2dc0fd9915cba138250a4e4fdf7bf38782304e
Alexander Mot <asd777101@gmail.com>
2021-04-12 18:16:25 -0700

    Change solution output from solved board to ArrayList of steps to reach solved board There are many issues here. Help.

--- src/rushhour/Solver.java
+++ src/rushhour/Solver.java
@@ -3,10 +3,8 @@ package rushhour;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.Stack;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Solver {
     public static void solveFromFile(String input, String output) {
@@ -19,40 +17,50 @@ public class Solver {
         writeToFile(solve(a), output);
     }
 
-    public static RushHour solve(RushHour board) {
+    public static ArrayList<char[]> solve(RushHour board) {
         return aStar(board);
     }
 
-    public static RushHour aStar(RushHour board) {
-        if (board.isSolved()) return board;
+    public static ArrayList<char[]> aStar(RushHour board) {
+        var stepsArrayList = new ArrayList<char[]>();
+        if (board.isSolved()) return stepsArrayList;    //if board is already solved, return an empty ArrayList
 
         HashSet<RushHour> visited = new HashSet<>();
         visited.add(board);
 
         var stack = new Stack<RushHour>();
         stack.add(board);
+
+        var map = new HashMap<RushHour, ArrayList<char[]>>();   //create map to link a board with the steps leading to it
+        map.put(board, stepsArrayList);                         //add board to map along with empty ArrayList
+
         while (!stack.isEmpty()){
             board = stack.pop();
-            for (RushHour newBoard : board.moves()) {
-                if (newBoard.isSolved()) {
-                    return newBoard;
-                }
+            for (RushHour newBoard : board.moves()) {           //iterate through possible next moves
                 if (!visited.contains(newBoard)){
                     visited.add(newBoard);
                     stack.add(newBoard);
+                    stepsArrayList = map.get(board);                //load the steps to the board
+                    stepsArrayList.add(board.boardDiff(newBoard));  //add steps to newBoard
+                    map.put(newBoard, stepsArrayList);              //add full steps to map for newBoard
                 }
+                if (newBoard.isSolved()) { return map.get(newBoard); }
             }
+            map.remove(board);      //remove parent board from the map once all children have been added
 
         }
         return null;
     }
 
-    public static void writeToFile(RushHour board, String filename) {
+    public static void writeToFile(ArrayList<char[]> steps, String filename) {
         try {
             var writer = new BufferedWriter(new FileWriter(filename));
-            writer.write(board.toString());
             System.out.println(filename);
-            System.out.println(board.toString());
+            for (char[] step : steps) {
+                writer.write(String.valueOf(step));
+                System.out.println(String.valueOf(step));
+                writer.newLine();
+            }
             writer.flush();
             writer.close();
         } catch (IOException e) {
